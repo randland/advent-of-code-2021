13e4da8fc10b0841392b4a44b8a85f734ae4c7e8
Nick Karpenske <randland@gmail.com>
2021-12-01 23:39:42 -0700

    :art: Day 1 (ruby) Better Output

--- day-01/ruby/main.rb
+++ day-01/ruby/main.rb
@@ -1,16 +1,11 @@
-def file(path)
-  File.read(File.join(__dir__, path))
-end
+def file(path) File.read(File.join(__dir__, path)); end
 
 def parse(data)
   data.split("\s").map &:to_i
 end
 
-EXAMPLE = parse file "example"
-INPUT = parse file "input"
-
-def count_inc(data, res)
-  (res...data.size).count { |i| data[i] > data[i - res] }
+def count_inc(data, delta)
+  (delta...data.size).count { |i| data[i] > data[i - delta] }
 end
 
 def part1(data)
@@ -21,9 +16,20 @@ def part2(data)
   count_inc(data, 3)
 end
 
-puts "*" * 80
-puts "Example 1: #{part1 EXAMPLE}"
-puts "Part 1: #{part1 INPUT}"
-puts "*" * 80
-puts "Example 2: #{part2 EXAMPLE}"
-puts "Part 2: #{part2 INPUT}"
+EXAMPLE = parse file "example"
+INPUT = parse file "input"
+
+puts <<~END
+##########
+# Part 1 #
+##########
+Example: #{part1 EXAMPLE}
+Solution: #{part1 INPUT}
+
+##########
+# Part 2 #
+##########
+Example: #{part2 EXAMPLE}
+Solution: #{part2 INPUT}
+
+END
