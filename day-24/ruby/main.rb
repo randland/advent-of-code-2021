698932728a4802f8a9f558973dec4d245dbdd345
Nick Karpenske <randland@gmail.com>
2021-12-30 01:17:12 -0700

    :sparkles: Day 24 (ruby)

--- day-24/ruby/main.rb
+++ day-24/ruby/main.rb
@@ -6,14 +6,78 @@ def file(path)
 end
 
 def parse(data)
-  data.split("\n\n")
+  data.split("\n").map { |r| r.split(" ") }
 end
 
-def part1(data)
-  data
+class SolverConstraint
+  attr_reader :a, :b, :n
+
+  def initialize(a, b, n)
+    @a, @b = n.positive? ? [a, b] : [b, a]
+    @n = n.abs
+  end
+
+  def min_vals
+    [[a, 1], [b, 1 + n]]
+  end
+
+  def max_vals
+    [[a, 9 - n], [b, 9]]
+  end
+
+  def inspect
+    "[#{a}] + #{n} == [#{b}]"
+  end
 end
 
-def part2(data)
+class ModelNumSolver
+  attr_reader :program
+
+  def initialize(program)
+    @program = program
+  end
+
+  def inst
+    return @inst if defined? @inst
+
+    chunks = program.chunk.with_index { |_, i| i / 18 }.map(&:last)
+
+    @inst = chunks.map do |c|
+              push = c[4][2] == "1"
+              [push, c[push ? 15 : 5][2].to_i]
+            end
+  end
+
+  def constraints
+    return @constraints if defined? @constraints
+
+    @constraints = []
+    queue = []
+    inst.each.with_index do |(push, val), idx|
+      next queue.push([idx, val]) if push
+
+      a_idx, a_val = queue.pop
+      @constraints << SolverConstraint.new(a_idx, idx, val + a_val)
+    end
+
+    @constraints
+  end
+
+  def max_num
+    constraints.flat_map(&:max_vals).sort_by(&:first).map(&:last).join
+  end
+
+  def min_num
+    constraints.flat_map(&:min_vals).sort_by(&:first).map(&:last).join
+  end
+end
+
+def part1(program)
+  ModelNumSolver.new(program).max_num
+end
+
+def part2(program)
+  ModelNumSolver.new(program).min_num
 end
 
 puts <<~END
@@ -22,8 +86,7 @@ puts <<~END
 # Part 1 #
 ##########
 END
-puts "Example: #{part1 parse file "example"}"
-# puts "Solution: #{part1 parse file "input"}"
+puts "Solution: #{part1 parse file "input"}"
 
 puts <<~END
 
@@ -31,5 +94,4 @@ puts <<~END
 # Part 2 #
 ##########
 END
-# puts "Example: #{part2 parse file "example"}"
-# puts "Solution: #{part2 parse file "input"}"
+puts "Solution: #{part2 parse file "input"}"
